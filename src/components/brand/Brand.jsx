b0cab60f475b273eefdfde4aca852134dc8b32a0
Patrick Mwila <patrickmwila.org@mail.com>
2023-06-10 10:56:35 +0200

    Modified Brand
    Designed the brand section component

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,28 @@
 import './brand.css'
+import { google, slack, atlassian, dropbox, shopify } from  './imports.js'
 
 const Brand = () => {
   return (
-    <div>
-      Brand
+    <div className="gpt3__brand">
+      <div>
+        <img src={ google } alt="google-img" />
+      </div>
+
+      <div>
+        <img src={ slack } alt="slack-img" />
+      </div>
+
+      <div>
+        <img src={ atlassian } alt="atlassian-img" />
+      </div>
+
+      <div>
+        <img src={ dropbox } alt="atlassian-img" />
+      </div>
+
+      <div>
+        <img src={ shopify } alt="shopify-img" />
+      </div>
     </div>
   )
 }
